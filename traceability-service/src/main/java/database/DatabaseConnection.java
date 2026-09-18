f7a8d156f2b38b10b8045af938913186d4c08a9b
Emil Tejn Rasmussen <emil@rasie.dk>
2026-09-18 14:05:16 +0200

    Add db credentials from a .envfiles

--- traceability-service/src/main/java/database/DatabaseConnection.java
+++ traceability-service/src/main/java/database/DatabaseConnection.java
@@ -1,15 +1,17 @@
 package database;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static final Dotenv dotenv = Dotenv.load();
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/slaughterhouse";
-
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
